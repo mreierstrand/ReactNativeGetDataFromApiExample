a4067f7891a43d319de0d4277e41faafcc547ad9
oliv1115 <48084927+oliv1115@users.noreply.github.com>
2020-11-25 11:08:35 +0100

    API Get

--- App.js
+++ App.js
@@ -1,21 +1,76 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, ActivityIndicator } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoading: true,
+      dataSource: null,
+    }
+  }
+  
+  componentDidMount() {
+    return fetch('https://raw.githubusercontent.com/facebook/react-native-website/master/website/static/movies.json')
+      .then ((response) => response.json())
+      .then ((responseJson) => {
+
+        this.setState({
+          isLoading: false,
+          dataSource: responseJson.movies,
+        })
+    })
+
+      .catch((error) => {
+      console.log(error)
+    });
+  }
+
+  render() {
+
+     if(this.state.isLoading) {
+
+       return (
+         <View style={styles.container}>
+          <StatusBar style="dark" />
+          <ActivityIndicator />
+         </View>
+       )
+     } else {
+
+      let movies = this.state.dataSource.map((val, key) => {
+        return <View key={key} style={styles.item}>
+                <Text>{val.title}</Text>
+                </View>
+      });
+
+        return (
+          <View style={styles.container}>
+            <StatusBar style="dark" />
+            {movies}
+          </View>
+          );
+        }
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#FFF',
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+  item: {
+    flex: 1,
+    alignSelf: 'stretch',
+    alignItems: 'center',
+    margin: 10,
+    justifyContent: 'center',
+    borderBottomWidth: 1,
+    borderBottomColor: '#eee'
+  }
+})
+
